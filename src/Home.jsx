e400378396a2121bc64c1cdd590e954bf931ab5c
lehojun <hojunchris@naver.com>
2025-03-26 14:06:00 +0900

    [FIX] #6 충돌 해결

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,21 +1,34 @@
 import React, { useState } from 'react'
-import Header from './components/home/header'
 import Sidebar from './components/home/Sidebar'
-import Chat from './components/home/Chat'
+import ChatArea from './components/home/ChatArea'
 import Inputbar from './components/home/Inputbar'
+import Header from './components/home/Header'
 import './css/home/home.css'
 
 export default function Home() {
+
+  const [messages, setMessages] = useState([]);
   const [isOpen, setOpen] = useState(true);
+
+  const handleSendMessage = (newMessage) => {
+    setMessages([
+      ...messages, 
+      { 
+        id: Date.now(), 
+        text: newMessage, 
+        sender: 'user' 
+      }
+    ]);
+  };
 
   return (
     <div className='home'>
-      {isOpen ? <Sidebar setOpen={setOpen}/> : <></>}
-      <div className='home-contents'>
-          <Header />
-          <Chat />
-          <Inputbar />
-      </div>
+    {isOpen ? <Sidebar setOpen={setOpen}/> : <></>}
+    <div className='home-contents'>
+            <Header />
+            <ChatArea messages={messages} />
+            <Inputbar onSendMessage={handleSendMessage}/>
+        </div>
     </div>
   )
 }
