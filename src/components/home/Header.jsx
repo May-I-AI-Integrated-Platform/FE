53ea3ff596ea66ac58819aefb6be90db713d0a8a
ohige01 <ohige01@naver.com>
2025-03-26 14:27:31 +0900

    [FIX] #8 사이드 바 토글 동작 수정

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
 import '../../css/home/header.css'
-const Header = () => {
+import srcSidebarIcon from '../../../image/sidebarIcon.png'
+
+const Header = (props) => {
 const [activeTab, setActiveTab] = useState('ChatGPT');
 
 const tabs = ['ChatGPT', 'Copilot', 'Bard', 'Claude'];
 
   return (
     <header className='header'>
+        {props.isOpen ? <></>: 
+          <div className='topIcon-sidebar' onClick={()=>{props.setOpen(true)}}>
+              <img src= {srcSidebarIcon}/>
+          </div>
+        }
         <div className='nav-tabs'>May I&nbsp;</div>
         <div className="ai-tabs">
           {tabs.map((tab) => (
