57e02866ee120a9730713cb3a47e1dd60a8d9b55
lehojun <hojunchris@naver.com>
2025-03-26 14:07:14 +0900

    [FEAT] #6 채팅 화면 구현

--- src/components/home/ChatArea.jsx
+++ src/components/home/ChatArea.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import '../../css/home/ChatArea.css';
 
-export default function Chat() {
+const ChatArea = ({ messages = [] }) => {
   return (
-    <div className='contents-chat'>Chat</div>
-  )
-}
+    <div className="chat-area">
+      <div className="chat-messages">
+        {messages.length > 0 ? (
+          messages.map((msg, index) => (
+            <div 
+              key={index} 
+              className={`message ${msg.sender === 'user' ? 'user-message' : 'bot-message'}`}
+            >
+              {msg.text}
+            </div>
+          ))
+        ) : (
+          <div className="no-messages">메시지가 없습니다.</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ChatArea;
