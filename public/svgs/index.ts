94b7288718d82ac43eac9df31ea8c37d844bb14f
이준희 <0210leejun@gmail.com>
2025-04-02 18:04:21 +0900

    💄 아이콘 추가

---
+++ public/svgs/index.ts
@@ -0,0 +1,10 @@
+export { default as KakaoIcon } from "./kakao_icon.svg";
+export { default as NaverIcon } from "./naver_icon.svg";
+export { default as GoogleIcon } from "./google_icon.svg";
+
+export { default as AddChatIcon } from "./add_chat_icon.svg";
+export { default as SidebarIcon } from "./sidebar_icon.svg";
+export { default as SendIcon } from "./send_icon.svg";
+export { default as SettingIcon } from "./setting_icon.svg";
+
+export { default as Logo } from "./logo.svg";
