05ded9ce1f1ad5e0fbdd0f25aef88252af73935b
이준희 <0210leejun@gmail.com>
2025-04-04 10:02:44 +0900

    💄 Storybook 생성

---
+++ src/stories/Chat/ChatInput.stories.tsx
@@ -0,0 +1,13 @@
+import ChatInput from '@/component/chat/ChatInput'; // 실제 컴포넌트 경로
+import type { Meta, StoryObj } from '@storybook/react';
+ 
+const meta: Meta<typeof ChatInput> = {
+  title: 'ChatInput',
+  component: ChatInput,
+  tags: ['autodocs'],
+};
+ 
+export default meta;
+type Story = StoryObj<typeof ChatInput>;
+ 
+export const Primary: Story = {};
