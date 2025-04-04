d3ad8d7fd957b7fdfce3e558f7bf51263b9d21e4
이준희 <0210leejun@gmail.com>
2025-04-04 14:30:22 +0900

    💄 도움말 아이콘 추가

--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -6,5 +6,6 @@ export { default as AddChatIcon } from "./add_chat_icon.svg";
 export { default as SidebarIcon } from "./sidebar_icon.svg";
 export { default as SendIcon } from "./send_icon.svg";
 export { default as SettingIcon } from "./setting_icon.svg";
+export { default as HelpIcon } from "./help_icon.svg";
 
 export { default as Logo } from "./logo.svg";
