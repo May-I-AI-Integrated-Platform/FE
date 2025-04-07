a4950ae5f248b0f11b2a089bd9443782192c59ad
jjjuni <0210leejun@gmail.com>
2025-04-07 15:59:45 +0900

    ✨ 유효성 검사 추가

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,13 +1,19 @@
 'use client'
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { GoogleIcon, KakaoIcon, Logo, NaverIcon } from "../../../public/svgs"
 import { useRouter } from "next/navigation";
+import { AnimatePresence, motion } from "framer-motion";
+import InputForm from "@/component/sign/InputForm";
 
 
 export default function Login() {
 
+  const emailReg = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+
   const router = useRouter();
+
+  const [isEmailValid, setIsEmailValid] = useState(true);
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -22,21 +28,29 @@ export default function Login() {
     }
   }
 
+  useEffect(() => {
+    if (emailReg.test(email)) {
+      setIsEmailValid(true)
+    } else {
+      setIsEmailValid(false)
+    }
+  }, [email])
+
   return (
     <div className={`w-screen h-screen flex flex-col items-center justify-center gap-5`}>
       <div className={`w-[40%] max-w-[500px] flex flex-col gap-10`}>
-        <Logo className={`cursor-pointer`} onClick={() => router.push('/')}/>
+        <Logo className={`cursor-pointer`} onClick={() => router.push('/')} />
 
         <div className={`flex flex-col gap-8 w-full`}>
-          <div className={`flex flex-col gap-2 `}>
-            <p className={`text-display-24-b`}>이메일</p>
-            <input
-              className={`border-b border-gray-500 text-body-16-r pb-1 placeholder:text-gray-300`}
-              placeholder="이메일을 입력해주세요"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)} 
-              onKeyDown={handleKeyDown}/>
-          </div>
+        <InputForm
+            title={"이메일"}
+            placeholder={"이메일을 입력해주세요"}
+            error={"이메일을 확인해주세요"}
+            value={email}
+            setValue={setEmail}
+            isValid={isEmailValid}
+            handleKeyDown={handleKeyDown} />
+            
           <div className={`flex flex-col gap-2 `}>
             <p className={`text-display-24-b`}>비밀번호</p>
             <input
@@ -44,8 +58,8 @@ export default function Login() {
               type="password"
               placeholder="비밀번호를 입력해주세요"
               value={password}
-              onChange={(e) => setPassword(e.target.value)} 
-              onKeyDown={handleKeyDown}/>
+              onChange={(e) => setPassword(e.target.value)}
+              onKeyDown={handleKeyDown} />
           </div>
         </div>
 
@@ -54,12 +68,12 @@ export default function Login() {
             <button
               className={`self-center rounded bg-point text-gray-900 w-full h-[48px] disabled:bg-gray-500 disabled:text-gray-50`}
               onClick={handleLogin}
-              disabled={email.trim() === "" || password.trim() === ""}>
+              disabled={!isEmailValid || password.trim() === ""}>
               로그인
             </button>
             <div className={`flex gap-2.5 items-center justify-center`}>
               <p className={`text-body-16-r`}>계정이 없으신가요?</p>
-              <p 
+              <p
                 className={`text-subhead-16-sb text-point cursor-pointer`}
                 onClick={() => router.push('/signup')}>회원가입</p>
             </div>
--- src/app/(beforeLogin)/signup/page.tsx
+++ src/app/(beforeLogin)/signup/page.tsx
@@ -1,22 +1,65 @@
 'use client'
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { Logo } from "../../../../public/svgs";
+import { AnimatePresence, motion } from "framer-motion";
+import InputForm from "@/component/sign/InputForm";
 
 
 export default function Signup() {
 
   const router = useRouter();
 
+  const emailReg = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+  const passwordReg = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordValid, setPasswordValid] = useState("");
 
+  const [isNameValid, setIsNameValid] = useState(true);
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+  const [isPasswordSame, setIsPasswordSame] = useState(true);
+
+
   const handleSignup = () => {
     router.push('/')
   }
+
+  useEffect(() => {
+    if (name.length > 1) {
+      setIsNameValid(true)
+    } else {
+      setIsNameValid(false)
+    }
+  }, [name])
+
+  useEffect(() => {
+    if (emailReg.test(email)) {
+      setIsEmailValid(true)
+    } else {
+      setIsEmailValid(false)
+    }
+  }, [email])
+
+  useEffect(() => {
+    if (passwordReg.test(password)) {
+      setIsPasswordValid(true)
+    } else {
+      setIsPasswordValid(false)
+    }
+  }, [password])
+
+  useEffect(() => {
+    if (password === passwordValid) {
+      setIsPasswordSame(true)
+    } else {
+      setIsPasswordSame(false)
+    }
+  }, [password, passwordValid])
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter" && name.trim() !== "" && email.trim() !== "" && password.trim() !== "" && passwordValid.trim() !== "") {
@@ -28,61 +71,61 @@ export default function Signup() {
     <div className={`w-screen h-screen flex flex-col items-center justify-center gap-5`}>
       <div className={`w-[40%] max-w-[500px] flex flex-col gap-10`}>
         <Logo className={`cursor-pointer`} onClick={() => router.push('/')} />
-        <div className={`flex flex-col gap-2 `}>
-          <p className={`text-display-24-b`}>이름</p>
-          <input
-            className={`border-b border-gray-500 text-body-16-r pb-1 placeholder:text-gray-300`}
-            placeholder="이름을 입력해주세요"
-            value={name}
-            onChange={(e) => setName(e.target.value)} 
-            onKeyDown={handleKeyDown}/>
-        </div>
         <div className={`flex flex-col gap-8 w-full`}>
-          <div className={`flex flex-col gap-2 `}>
-            <p className={`text-display-24-b`}>이메일</p>
-            <input
-              className={`border-b border-gray-500 text-body-16-r pb-1 placeholder:text-gray-300`}
-              placeholder="이메일을 입력해주세요"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)} 
-              onKeyDown={handleKeyDown}/>
-          </div>
-          <div className={`flex flex-col gap-2 `}>
-            <p className={`text-display-24-b`}>비밀번호</p>
-            <input
-              className={`border-b border-gray-500 text-body-16-r pb-1 placeholder:text-gray-300`}
-              type="password"
-              placeholder="비밀번호를 입력해주세요"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)} 
-              onKeyDown={handleKeyDown}/>
-          </div>
-          <div className={`flex flex-col gap-2 `}>
-            <p className={`text-display-24-b`}>비밀번호 확인</p>
-            <input
-              className={`border-b border-gray-500 text-body-16-r pb-1 placeholder:text-gray-300`}
-              type="password"
-              placeholder="비밀번호를 재입력해주세요"
-              value={passwordValid}
-              onChange={(e) => setPasswordValid(e.target.value)} 
-              onKeyDown={handleKeyDown}/>
-          </div>
-        </div>
 
-        <div className={`flex flex-col gap-[60px]`}>
-          <div className={`flex flex-col gap-5 w-full`}>
-            <button
-              className={`self-center rounded bg-point text-gray-900 w-full h-[48px] disabled:bg-gray-500 disabled:text-gray-50`}
-              disabled={
-                name.trim() === "" ||
-                email.trim() === "" || 
-                password.trim() === "" || 
-                passwordValid.trim() === ""}
-              onClick={handleSignup}>
-              확인
-            </button>
-          </div>
+          <InputForm
+            title={"이름"}
+            placeholder={"이름을 입력해주세요"}
+            error={"이름은 2자 이상이어야 합니다"}
+            value={name}
+            setValue={setName}
+            isValid={isNameValid}
+            handleKeyDown={handleKeyDown} />
 
+          <InputForm
+            title={"이메일"}
+            placeholder={"이메일을 입력해주세요"}
+            error={"이메일을 확인해주세요"}
+            value={email}
+            setValue={setEmail}
+            isValid={isEmailValid}
+            handleKeyDown={handleKeyDown} />
+
+          <InputForm
+            title={"비밀번호"}
+            placeholder={"비밀번호를 입력해주세요"}
+            error={"비밀번호는 영문/숫자/특수기호 조합 8자리 이상이어야 합니다"}
+            value={password}
+            setValue={setPassword}
+            isValid={isPasswordValid}
+            handleKeyDown={handleKeyDown} 
+            isPassword={true}/>
+
+          <InputForm
+            title={"비밀번호 확인"}
+            placeholder={"비밀번호를 재입력해주세요"}
+            error={"비밀번호가 일치하지 않습니다"}
+            value={passwordValid}
+            setValue={setPasswordValid}
+            isValid={isPasswordSame}
+            handleKeyDown={handleKeyDown} 
+            isPassword={true}/>
+
+          <div className={`flex flex-col gap-[60px]`}>
+            <div className={`flex flex-col gap-5 w-full`}>
+              <button
+                className={`self-center rounded bg-point text-gray-900 w-full h-[48px] disabled:bg-gray-500 disabled:text-gray-50`}
+                disabled={
+                  name.trim() === "" ||
+                  email.trim() === "" || !isEmailValid ||
+                  password.trim() === "" || !isPasswordValid ||
+                  passwordValid.trim() === "" || !isPasswordSame}
+                onClick={handleSignup}>
+                확인
+              </button>
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
